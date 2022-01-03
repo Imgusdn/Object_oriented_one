0c9ea67363bb37dbd3e03b8c19c1598e9f74e9fa
ejffoddl1 <ejffoddl1@naver.com>
2022-01-04 00:05:54 +0900

    (220104) quiz01 객체구현 2문제

---
+++ src/quiz01/PersonTest.java
@@ -0,0 +1,10 @@
+package quiz01;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Person tomas = new Person(180, 78, "³²¼º", "Tomas", 37);
+
+        System.out.println(tomas.showInfo());
+    }
+}
