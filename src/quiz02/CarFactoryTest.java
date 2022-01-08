5523aa721bd19666c000ae6c8d879e9bc482a7c7
ejffoddl1 <ejffoddl1@naver.com>
2022-01-08 20:14:22 +0900

    (220108) static 싱글톤 패턴

---
+++ src/quiz02/CarFactoryTest.java
@@ -0,0 +1,7 @@
+package quiz02;
+
+public class CarFactoryTest {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/singletonpattern/CompaneyTest.java
@@ -0,0 +1,16 @@
+package singletonpattern;
+
+import java.util.Calendar;
+
+public class CompaneyTest {
+	public static void main(String[] args) {
+		
+		Company company1 = Company.getInstance();
+		Company company2 = Company.getInstance();
+		
+		System.out.println(company1);
+		System.out.println(company2);
+		
+		Calendar calendar = Calendar.getInstance();
+	}
+}
